9b35f3c99cf3db646debc5beb37880280519d2a6
Mohammed <melamin100@yahoo.com>
2023-07-13 02:03:12 +0400

    Initial Commit

---
+++ calmo/calmoApp.swift
@@ -0,0 +1,17 @@
+//
+//  calmoApp.swift
+//  calmo
+//
+//  Created by Mohammed Elamin on 13/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct calmoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
