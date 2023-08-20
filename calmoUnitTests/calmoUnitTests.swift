1b8699b409c124e738275c9696b4fe9ce47c7fe2
Mohammed <melamin100@yahoo.com>
2023-08-20 14:24:52 +0400

    prepare fastlane to run testings uising actins

---
+++ calmoUnitTests/calmoUnitTests.swift
@@ -0,0 +1,26 @@
+//
+//  calmoUnitTests.swift
+//  calmoUnitTests
+//
+//  Created by Mohammed Elamin on 20/08/2023.
+//
+
+import XCTest
+
+final class calmoUnitTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testFunction() throws {
+       XCTAssertTrue(1==1)
+    }
+
+   
+
+}
